7e9be8c49d745160e6d303c0553bdda47d329f85
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-12-02 23:14:17 -0300

    Começo do home, com h1 e espaçamentos do main

---
+++ view/pages/home.php
@@ -0,0 +1,1 @@
+<h1 class="mx-auto">Transforme seus 💚 Desejos em Presentes Memoráveis 🎅🏼</h1>
--- view/template/template_nav.php
+++ view/template/template_nav.php
@@ -21,7 +21,6 @@
     <div class="container-fluid">
 
         <nav class="navbar navbar-expand-lg navbar-light">
-
             <a href=""><img src="view/images/logoMedium.png" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
@@ -46,13 +45,15 @@
                     </ul>
                     <button type="button" class="btn btn-success">Cadastre</button>
                 </div>
-            </div>
 
+            </div>
         </nav>
 
+        <main>
+            <?php require "view/pages/$page.php" ;?>
+        </main>
+
     </div>
-    
-    <?php require "view/pages/$page.php" ;?>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
