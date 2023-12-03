a4acb257859b6fbc5b225ad4374c86c2bf69ce49
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-12-02 22:13:51 -0300

    Conexão com bd e classe item com método construtor

---
+++ model/Connection.php
@@ -0,0 +1,23 @@
+<?php
+namespace GiftWish\Model;
+use \PDO;
+use \PDOException;
+
+class Connection extends PDO{
+    private $banco='GiftWish';
+    private $user='root';
+    private $password='';
+
+    function __construct()
+    {
+        $dns = 'mysql:host=localhost; dbname='. $this -> banco;
+        try{
+            parent::__construct($dns, $this->user, $this -> password);
+        } catch (PDOException $e){
+            file_put_contents("log.txt", $e->getMessage(), FILE_APPEND | LOCK_EX);
+            die("Erro ao conectar no banco de dados. Tente novamente");
+        }
+    }
+}
+
+?>
--- model/Item.php
+++ model/Item.php
@@ -1,7 +1,34 @@
 <?php
+namespace GifiWish\Model;
 
 class Item{
     
+    public $id;
+    public $nome;
+    public $descricao;
+    public $categoria;
+    public $nivel_desejo;
+    public $media_preco;
+
+    function __construct($id=null){
+        if($id){
+            $conn = new Connection();
+            $user = $conn -> query("SELECT * FROM item WHERE id=$id");
+
+            if($item){
+                $this -> id = $item['id'];
+                $this -> nome = $item['nome'];
+                $this -> descricao = $item['descricao'];
+                $this -> categoria = $item['categoria'];
+                $this -> nivel_desejo = $item['nivel_desejo'];
+                $this -> media_preco = $item['media_preco'];
+            }else{
+                die('Item não encontrado');
+            }
+        }
+        
+    }
+
 }
 
 ?>
