e195e6edbeb5bd80ac4f97ceb657f9d5c205cb0f
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-12-04 21:32:38 -0300

    Correções no banco de dados

--- database.sql
+++ database.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `item` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `nome` char(50) NOT NULL,
   `descricao` text NOT NULL,
   `categoria` char(50) NOT NULL,
@@ -44,8 +44,8 @@ INSERT INTO `item` (`id`, `nome`, `descricao`, `categoria`, `nivel_desejo`, `med
 (1, 'Caderno', 'Gostaria de um caderno de 20 matérias para eu começar as aulas semana que vem, de preferência de capa de cor rosa', 'Material Escolar', 'Médio', '50.00'),
 (2, 'Computador', 'Meu atual computador esta com defeito, portanto, necessito  de um novo, desde que seja da Dell', 'Eletrônico', 'Alto', '2000.00'),
 (3, 'Mouse', 'Um mouse bluetooth com leds é o que falta para meu maravilhoso setup gamer. Gostaria de fosse branco, para combinar ainda mais com o meu setup', 'Eletrônico', 'Alto', '200.00'),
-(4, 'Sapato', 'Urgente, para o evento que acontecerá nesse fim de semana,que é uma festa formal', 'Vestuário', 'Médio', '200.00');
-(5, 'Morro dos Ventos Uivantes', 'Eu gosto muito desse tipo de romance. Gostaria de aumentar minha coleção de livros desta categoria', 'Livro', 'Médio', '30.00');
+(4, 'Sapato', 'Urgente, para o evento que acontecerá nesse fim de semana,que é uma festa formal', 'Vestuário', 'Médio', '200.00'),
+(5, 'Morro dos Ventos Uivantes', 'Eu gosto muito desse tipo de romance. Gostaria de aumentar minha coleção de livros desta categoria', 'Livro', 'Médio', '30.00'),
 (6, 'Monitor', 'Só falta para meu setup um monitor. Pode ser qualquer tamanho', 'Eletrônico', 'Médio', '200.00');
 
 --
@@ -54,14 +54,6 @@ INSERT INTO `item` (`id`, `nome`, `descricao`, `categoria`, `nivel_desejo`, `med
 
 --
 -- Índices para tabela `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
 --
 -- AUTO_INCREMENT de tabela `item`
 --
