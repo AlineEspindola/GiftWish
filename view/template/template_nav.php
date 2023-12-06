8723cced858256f145516a0ee08f25bdda48fc82
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2023-12-05 21:09:07 -0300

    Adicionado descrição nos alt das imagens

--- view/template/template_nav.php
+++ view/template/template_nav.php
@@ -23,7 +23,7 @@
     <div class="container-fluid">
 
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a href=""><img src="view/images/logoMedium.png" alt=""></a>
+            <a href=""><img src="view/images/logoMedium.png" alt="Logo da GiftWish"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
